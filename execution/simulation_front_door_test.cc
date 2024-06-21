7c994d9680251e29949d948bc1f0b7c4dbd01aac
Conner Hansen <hansenconners@gmail.com>
2024-06-20 17:42:48 -0700

    add model and simulation to tests; this serves just to force me to start exposing headers where they will be necessary

--- execution/simulation_front_door_test.cc
+++ execution/simulation_front_door_test.cc
@@ -2,13 +2,18 @@
 
 #include <string>
 
+#include "model.h"
 #include "simulation.h"
 
+using namespace execution;
+using namespace model;
 using namespace std;
 
 class SimulationTest : public testing::TestWithParam<string> {
 	protected:
-
+	Simulation *make_simulation(Model *model) {
+		return nullptr;
+	};
 };
 
 INSTANTIATE_TEST_SUITE_P(
@@ -19,4 +24,6 @@ INSTANTIATE_TEST_SUITE_P(
 
 
 TEST_P(SimulationTest, PlaceHolder) {
+	Model *model = nullptr;
+	make_simulation(model);
 }
--- model/model.h
+++ model/model.h
@@ -6,7 +6,6 @@
 namespace model {
 
 	class Model {
-		public:
 	};
 
 } // model
