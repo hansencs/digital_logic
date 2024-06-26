44e2b502d3fc2e6a6de6ec856e5ee7348952fbb5
Conner Hansen <hansenconners@gmail.com>
2024-06-26 11:32:24 -0700

    cleanup: delete some files from before the large refactor

--- src/model/dual_pin.cc
+++
@@ -1,8 +0,0 @@
-#include "dual_pin.h"
-
-using namespace model;
-using namespace std;
-
-DualPin::DualPin(const string &name, const Component *component) :
-	InputPin(name, component), OutputPin(name, component)
-{}
